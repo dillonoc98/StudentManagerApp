5e0fdf42d4317c57c5419f3889d92775a67e347b
dillonoc98 <dillonocallaghan@gmail.com>
2018-10-25 13:45:29 +0100

    original state

--- src/ie/gmit/studentmanager/Runner.java
+++ src/ie/gmit/studentmanager/Runner.java
@@ -3,7 +3,6 @@ package ie.gmit.studentmanager;
 public class Runner {
     
     public static void main(String[] args){
-         System.out.println("yo");
          Menu menu1 = new Menu();
         menu1.display();
        
